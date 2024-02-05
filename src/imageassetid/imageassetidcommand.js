789b5592bea9f6825d5de5f8146936096e69004d
Cobzaru Luca <luca@studyly.com>
2024-02-05 11:31:34 +0100

    STU-1978: STU-1976 initial name changed commit

--- src/imageassetid/imageassetidcommand.js
+++ src/imageassetid/imageassetidcommand.js
@@ -2,10 +2,9 @@ import Command from '@ckeditor/ckeditor5-core/src/command';
 /**
  * The image resize command. Currently, it supports both the width and the height attributes.
  */
-export default class ImageCopyrightCommand extends Command {
-
-    constructor( editor ) {
-        super( editor );
+export default class ImageAssetIdCommand extends Command {
+    constructor(editor) {
+        super(editor);
         this.value = null;
     }
 
@@ -17,15 +16,16 @@ export default class ImageCopyrightCommand extends Command {
      * @inheritDoc
      */
     refresh() {
-        const imageUtils = this.editor.plugins.get( 'ImageUtils' );
-        const element = this.editor.model.document.selection.getSelectedElement();
-        this.isEnabled = imageUtils.isImage( element );
+        const imageUtils = this.editor.plugins.get('ImageUtils');
+        const element =
+            this.editor.model.document.selection.getSelectedElement();
+        this.isEnabled = imageUtils.isImage(element);
 
-        let copyrightNotice = this.getCopyrightNotice( element );
+        let copyrightNotice = this.getCopyrightNotice(element);
 
         if (copyrightNotice) {
             this.value = {
-                'copyright-notice': copyrightNotice
+                'copyright-notice': copyrightNotice,
             };
         } else {
             this.value = null;
@@ -34,8 +34,8 @@ export default class ImageCopyrightCommand extends Command {
 
     getCopyrightNotice(element) {
         let copyright = null;
-        if ( element && element.hasAttribute( 'copyright-notice' ) ) {
-            copyright = element.getAttribute( 'copyright-notice' );
+        if (element && element.hasAttribute('copyright-notice')) {
+            copyright = element.getAttribute('copyright-notice');
         }
         return copyright;
     }
@@ -45,17 +45,17 @@ export default class ImageCopyrightCommand extends Command {
      * @param {Object} options
      * @param {String|null} options['max-width'] The max-width of the image.
      */
-    execute( options ) {
+    execute(options) {
         const model = this.editor.model;
         const imageElement = model.document.selection.getSelectedElement();
 
-        model.change( writer => {
+        model.change((writer) => {
             if (options['copyright-notice']) {
                 writer.setAttribute(
                     'copyright-notice',
                     options['copyright-notice'],
                     imageElement
-                )
+                );
             } else {
                 writer.removeAttribute('copyright-notice', imageElement);
             }
--- src/imageassetid/imageassetidui.js
+++ src/imageassetid/imageassetidui.js
@@ -1,19 +1,18 @@
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
-import InputTextView from "@ckeditor/ckeditor5-ui/src/inputtext/inputtextview";
+import InputTextView from '@ckeditor/ckeditor5-ui/src/inputtext/inputtextview';
 
-import '../../theme/image-copyright.css';
+import '../../theme/image-asset-id.css';
 
 /**
  * The image style UI plugin.
  */
-export default class ImageCopyrightUi extends Plugin {
+export default class ImageAssetIdUi extends Plugin {
     /**
      * @inheritDoc
      */
     static get pluginName() {
-        return 'ImageCopyrightUi';
+        return 'ImageAssetIdUi';
     }
-
 
     /**
      * @inheritDoc
@@ -21,7 +20,6 @@ export default class ImageCopyrightUi extends Plugin {
     init() {
         this._createInput();
     }
-
 
     /**
      * Creates an input for image-copyright and stores it in the editor
@@ -31,41 +29,39 @@ export default class ImageCopyrightUi extends Plugin {
         const editor = this.editor;
         const componentName = `imageCopyright`; //TODO refactor this to imageCopyrightInput
 
-        editor.ui.componentFactory.add( componentName, locale => {
-            const command = editor.commands.get( 'imageCopyright' );
-            const input = new InputTextView( locale );
+        editor.ui.componentFactory.add(componentName, (locale) => {
+            const command = editor.commands.get('imageCopyright');
+            const input = new InputTextView(locale);
             const t = locale.t;
 
-            input.set( {
+            input.set({
                 placeholder: t('copyright'),
-            } );
+            });
 
             input.extendTemplate({
                 attributes: {
-                    class: [
-                        'image-copyright-input'
-                    ]
-                }
-            })
+                    class: ['image-copyright-input'],
+                },
+            });
 
-            input.bind( 'value' ).to( command, (value) => {
+            input.bind('value').to(command, (value) => {
                 return value ? value['copyright-notice'] : null;
-            } );
+            });
 
             input.on('input', () => {
                 // used instead of input.isEmpty because after typing one char into empty input, it doesn't register as not empty
                 if (input.element.value.length === 0) {
-                    editor.execute( 'imageCopyright', {
-                        'copyright-notice': null
+                    editor.execute('imageCopyright', {
+                        'copyright-notice': null,
                     });
                 } else {
                     editor.execute('imageCopyright', {
-                        'copyright-notice': input.element.value
+                        'copyright-notice': input.element.value,
                     });
                 }
-            } );
+            });
 
             return input;
-        } );
+        });
     }
 }
